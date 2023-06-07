15d4bf64dd81f78b211fad71da1e8e034988636d
Mert Akdoğan2 <65607835+mertakd@users.noreply.github.com>
2023-06-07 17:14:41 +0300

    lesson 1 Andromeda Variable.kt

---
+++ src/main/kotlin/JetBrainsAcademy/Andromeda/Variable.kt
@@ -0,0 +1,139 @@
+package JetBrainsAcademy.Andromeda
+
+/*
+fun main(){
+
+
+//yeniders
+//val->valuevar->variable
+//varsonradandeğişebilir.valdeğerdemekbirkereatıyorsundeğişmemişoluyor.
+//ilkbaştavalolaraktanımlamamızlazımbestpractiseteknikborç.
+//accessmodifier->val/var-<name//Type
+//typeinferencedeğişkenintipinikendibilebiliyorkotlin
+//valintegerType:Int=1976
+
+//creditcard,vatadaşlıknumarası,veritabanınumaralarındalongkullanılmalı
+    valintegerType:Long=8000000000000000000
+
+//UnsignedIntegerbak
+
+//isimlerdirmeleranlamlıolsun.number,double,fun,classgibiisimlerdirmeyapma.
+
+//double
+    valpi:Double=1.45//virgülolmayacak
+    vale:Float=2.4564645f//7basamağakadartanımlayabiliyor.büyükhesaplamalardadoublekullan.
+
+//BigDouble->dahabüyükhesaplardakullanılabilir.
+
+//1.248,08$1,248.08$1000000.00<-buşekildedönüştürmemizlazım.localegöreuygunhaleçevirmemizgerekiyor.bunulocalegöreçevirenfonksiyonlarvar.
+
+//Hexadecinmal0X0F
+
+//binary0b0001
+
+//valmillion=1_000_000->bestpractise
+
+
+//hergelenveriyenullkullanılmaz.
+
+
+//-128ile127bellekoptimizasyonu
+//vara:Int?=10
+//valb:Int=10
+
+    */
+/*if(a===b){
+
+    }*//*
+
+
+    vara:Int?=null
+    a=10
+    valb:Int=10
+
+
+//boolean
+    valisOdd=false
+    if(isOdd.not()||!isOdd){
+
+    }
+
+
+//String
+//mutrableimmutable
+    varfullName="Mert"
+    fullName="Murat"
+
+    fullName.forEach{}
+
+    valupperCase=fullName.toUpperCase()
+
+    println(fullName)
+    println(upperCase)
+
+
+
+
+//Arrays
+    valnumbers=intArrayOf(1,2,3,4,5)
+    valgrades=floatArrayOf(24.36f,25.32f)
+    valnumbersOther=Array<Int>(5){index->
+        println(index)
+        0
+
+    }
+
+    */
+/*numbers.forEachIndexed{index,->
+
+
+    }*//*
+
+
+    println(numbersOther.toString())
+
+//kotlindeenüstsınıfantjavadaobject
+    valmixedElements=arrayOf("mert",12)
+
+    valmixed2=arrayOfNulls<Int>(5)
+
+
+
+//anyyistringeçevirmek
+    valx:Any=""
+
+    if(xisString){
+        x.length
+    }else{
+//x.length
+    }
+
+
+    if(x!isString){
+//x.equals()
+    }else{
+//.length
+    }
+
+
+    if(x!isString)return
+    x.length
+
+
+    valy:Any?=null
+    valcasted:String?=yas?String
+    valcasted2:String?=yasString?
+
+
+    fun(){
+        println("yeniders")
+    }
+
+
+
+
+
+
+
+}
+*/
